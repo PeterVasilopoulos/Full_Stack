deeb36d8deaf4f31269f2624c27aac650fa79b10
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-05-12 12:20:08 -0400

    Updated env and config file

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,3 +1,15 @@
 // Establish a connection to db server
 const mongoose = require("mongoose");
 
+const dbName = process.env.ATLAS_DATABASE;
+const username = process.env.ATLAS_USERNAME;
+const pw = process.env.ATLAS_PASSWORD;
+
+const uri = `mongodb+srv://${username}:${pw}@pogchamp.ist0h13.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log(`Established a comm link to DS Port Maintenance ${dbName}`))
+.catch((err) => console.log('Abort! The Rebel Alliance is boarding the space station!', err))
